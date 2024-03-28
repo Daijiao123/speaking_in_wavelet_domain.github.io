f93f2de0b419416b52f05e12ce54e425d5d714a3
Daijiao123 <liuan202206@gmail.com>
2024-03-28 22:55:09 +1100

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+// 基本的JavaScript代码（如果需要）
+document.addEventListener('play', function(e){
+    var audios = document.getElementsByTagName('audio');
+    for(var i = 0, len = audios.length; i < len;i++){
+        if(audios[i] != e.target){
+            audios[i].pause();
+        }
+    }
+}, true);
